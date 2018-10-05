5ebb653dd31c1d08a1dc614a0428d5ca4cdce632
Andrew McCarthy <andrewkmccarthy13@gmail.com>
2018-10-05 17:10:52 -0500

    feat(socket): initial socket implementation

---
+++ cmake-build-debug/CMakeFiles/snc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snc.dir/snc.c.o"
+  "snc.pdb"
+  "snc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/snc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ snc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <strings.h>
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+
+int main(int argc, char *argv[]) {
+    int sockfd, newsockfd, portno, clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    if (argc < 2) {
+        fprintf(stderr, "ERROR, no port provided");
+        exit(1);
+    }
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+}
