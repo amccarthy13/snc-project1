c5a341c0eeb1cb100e1b6aa8208cd1c5b15d5b4a
Andrew McCarthy <andrewkmccarthy13@gmail.com>
2018-10-05 19:02:40 -0500

    fix(socket): fixed bugs in socket

--- snc.c
+++ snc.c
@@ -10,9 +10,9 @@
 void error(char *msg)
 {
     perror(msg);
-    printf("\n");
     exit(1);
 }
+
 
 int main(int argc, char *argv[]) {
     int sockfd, newsockfd, portno, clilen;
@@ -20,19 +20,29 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in serv_addr, cli_addr;
     struct hostent *server;
     int n;
-    if (argc < 2) {
+    char* type_flag = argv[1];
+    int protocol;
+    int flag = 0;
+    for (int i = 1; i <= 2; i++) {
+        if (strcmp(argv[i], "u") == 0) {
+            protocol = IPPROTO_UDP;
+            flag = 1;
+        }
+    }
+    if (!flag) {
+        protocol = IPPROTO_TCP;
+    }
+    if (argc < 3) {
         fprintf(stderr, "invalid or missing options\n"
                         "usage: snc [-l] [-u] [hostname] port");
         exit(1);
     }
-    if (argv[1] == 'l') {
-        int protocol;
-        if (argv[2] == (char *) 'u') protocol = IPPROTO_UDP; else protocol = IPPROTO_TCP;
+    if ((strcmp("l", type_flag) == 0)) {
         sockfd = socket(AF_INET, SOCK_STREAM, protocol);
         if (sockfd < 0 )
             error("internal error");
         bzero((char *) &serv_addr, sizeof(serv_addr));
-        portno = atoi(argv[argc]);
+        portno = atoi(argv[argc - 1]);
         serv_addr.sin_family = AF_INET;
         serv_addr.sin_addr.s_addr = INADDR_ANY;
         serv_addr.sin_port = htons(portno);
@@ -41,7 +51,7 @@ int main(int argc, char *argv[]) {
             error("internal error");
         listen(sockfd, 5);
         clilen = sizeof(cli_addr);
-        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *) &clilen);
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
         if (newsockfd < 0)
             error("internal error");
         bzero(buffer, 256);
@@ -52,23 +62,15 @@ int main(int argc, char *argv[]) {
         if (n < 0) error("internal error");
         return 0;
     }
-
     else {
-        char local_buffer[256];
-        if (argc < 3) {
-            fprintf(stderr, "invalid or missing options\n"
-                            "usage: snc [-l] [-u] [hostname] port");
-            exit(1);
-        }
-        portno = atoi(argv[argc]);
-        int protocol;
-        if (argv[2] == (char *) 'u') protocol = IPPROTO_UDP; else protocol = IPPROTO_TCP;
+        char buffer[256];
+        portno = atoi(argv[argc - 1]);
         sockfd = socket(AF_INET, SOCK_STREAM, protocol);
         if (sockfd < 0)
             error("internal error");
-        server = gethostbyname(argv[argc - 1]);
+        server = gethostbyname(argv[argc - 2]);
         if (server == NULL) {
-            fprintf(stderr,"internal error help me");
+            fprintf(stderr,"internal error\n");
             exit(0);
         }
         bzero((char *) &serv_addr, sizeof(serv_addr));
@@ -80,16 +82,16 @@ int main(int argc, char *argv[]) {
         if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
             error("internal error");
         printf("Please enter the message: ");
-        bzero(local_buffer,256);
-        fgets(local_buffer,255,stdin);
-        n = (int) write(sockfd, local_buffer, strlen(local_buffer));
+        bzero(buffer,256);
+        fgets(buffer,255,stdin);
+        n = (int) write(sockfd, buffer, strlen(buffer));
         if (n < 0)
             error("internal error");
-        bzero(local_buffer,256);
-        n = (int) read(sockfd, local_buffer, 255);
+        bzero(buffer,256);
+        n = (int) read(sockfd, buffer, 255);
         if (n < 0)
             error("internal error");
-        printf("%s\n",local_buffer);
+        printf("%s\n",buffer);
         return 0;
     }
 }
